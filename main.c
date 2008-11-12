652e038d73c5723b7d3e1d4ac4bff3d9e39226b8
Jeff Garzik <jeff@garzik.org>
2008-11-12 02:25:55 -0500

    Import netbsd-iscsi (originally Intel) BSD-licensed iSCSI target code.

--- main.c
+++ main.c
@@ -1,8 +1,33 @@
 
 #include <glib.h>
+#include <stdint.h>
+#include <netinet/in.h>
 
-int main (int argc, char *argv[])
+#include "compat.h"
+#include "iscsi.h"
+#include "target.h"
+#include "device.h"
+#include "md5.h"
+#include "parameters.h"
+
+uint32_t iscsi_debug_level = 0;
+
+int device_init(globals_t * a, targv_t * b, disc_target_t * c)
+{
+	return -1;
+}
+
+int device_command(target_session_t * e, target_cmd_t * d)
+{
+	return -1;
+}
+
+int device_shutdown(target_session_t * f)
+{
+	return -1;
+}
+
+int main(int argc, char *argv[])
 {
 	return 0;
 }
-
